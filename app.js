a81e7280554f82bb992e06840749eb77e61174aa
Federico2235 <fedeand3@gmail.com>
2024-12-30 16:27:45 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,14 @@
+for (let i = 1; i <= 3; i++) {
+  let extend = document.getElementById(`extend${i}`);
+  let expand = document.getElementById(`expand${i}`);
+  let extendImg = extend.querySelector("img");
+  extend.addEventListener("click", function () {
+    if (expand.classList.contains("open")) {
+      expand.classList.remove("open");
+      extendImg.classList.remove("rotated");
+    } else {
+      expand.classList.add("open");
+      extendImg.classList.add("rotated");
+    }
+  });
+}
