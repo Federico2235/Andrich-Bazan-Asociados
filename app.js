6f40f7495c94eefda66f1663b91d658d4e1ee81b
Federico2235 <fedeand3@gmail.com>
2025-06-07 17:11:46 +0200

    Mi versión local actual para subir al remoto

--- app.js
+++ app.js
@@ -13,3 +13,35 @@ for (let i = 1; i <= 3; i++) {
   });
 }
 
+  for (let i = 1; i <= 3; i++) {
+    let extendC = document.getElementById(`extendC${i}`);
+    let expandC = document.getElementById(`expandC${i}`);
+    let extendImg = extendC.querySelector("img");
+    extendC.addEventListener("click", function () {
+      if (expandC.classList.contains("open")) {
+        expandC.classList.remove("open");
+        extendImg.classList.remove("rotated");
+      } else {
+        expandC.classList.add("open");
+        extendImg.classList.add("rotated");
+      }
+    });
+  }
+  
+
+  
+    const track = document.querySelector('.carousel-track');
+    const nextBtn = document.querySelector('.carousel-btn.next');
+    const prevBtn = document.querySelector('.carousel-btn.prev');
+    const cardCar = document.querySelector('.cardCar');
+    const gap = 10; // gap de 1rem
+    const cardWidth = cardCar.offsetWidth + gap;
+
+    nextBtn.addEventListener('click', () => {
+      track.scrollBy({ left: cardWidth, behavior: 'smooth' });
+    });
+
+    prevBtn.addEventListener('click', () => {
+      track.scrollBy({ left: -cardWidth, behavior: 'smooth' });
+    });
+
